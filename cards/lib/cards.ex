b94cd7dc50223e1ecfad6541fac9468c25093566
Francisco Guilherme <francisco.antunes@pravaler.com.br>
2021-10-15 20:35:13 -0300

    feat(poc): implementa alguns metodos.

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -1,18 +1,9 @@
 defmodule Cards do
-  @moduledoc """
-  Documentation for `Cards`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Cards.hello()
-      :world
-
-  """
   def hello do
-    :world
+    "hi there!"
+  end
+
+  def create_deck do
+    ["Ace", "Two", "Three"]
   end
 end
