f339fb4e4e29b13fc4ff931903fc1757f4a71f18
Francisco Guilherme <francisco.antunes@pravaler.com.br>
2021-10-16 14:41:49 -0300

    feat(poc): implementa um modulo para estudo de maps.

---
+++ cards/lib/struct.ex
@@ -0,0 +1,20 @@
+defmodule Struct do
+  def maps_structure do
+    colors = %{primary: "red", secondary: "blue"}
+    %{secondary: secondary_color} = colors
+    secondary_color
+  end
+
+  def get_map do
+    %{pai: "P", mae: "M", filha: "F"}
+  end
+
+  def update_maps_function(map_data, key, value) do
+    Map.put(map_data, key, value)
+  end
+
+  def update_maps_sintax do
+    colors = %{primary: "red", secondary: "blue"}
+    %{ colors | primary: "yellow" }
+  end
+end
