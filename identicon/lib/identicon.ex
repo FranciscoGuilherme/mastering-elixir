32035aba82eae49add6b623f9ddd41157fe36123
Francisco Guilherme <francisco.antunes@pravaler.com.br>
2021-10-16 16:43:14 -0300

    feat(poc): implementa uma estrutura.

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -1,11 +1,15 @@
 defmodule Identicon do
+  alias Identicon.Image
+
   def main(input) do
     input
     |> hash_input
   end
 
   def hash_input(input) do
-    :crypto.hash(:md5, input)
+    hex = :crypto.hash(:md5, input)
     |> :binary.bin_to_list
+
+    %Image{hex: hex}
   end
 end
