e4d893f2738fd8db3cd7705dfcd507a185f2e49c
Francisco Guilherme <francisco.antunes@pravaler.com.br>
2021-10-15 21:14:10 -0300

    feat(poc): implementa metodos de demonstracao de list comprehensions.

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -4,6 +4,36 @@ defmodule Cards do
   end
 
   def create_deck do
-    ["Ace", "Two", "Three"]
+    values = ["Ace", "Two", "Three", "Four", "Five"]
+    suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
+
+    for suit <- suits do
+      for value <- values do
+        "#{value} of #{suit}"
+      end
+    end
+  end
+
+  def create_deck_ok do
+    values = ["Ace", "Two", "Three", "Four", "Five"]
+    suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
+
+    for suit <- suits, value <- values do
+      "#{value} of #{suit}"
+    end
+  end
+
+
+  def solving do
+    create_deck()
+    |> List.flatten()
+  end
+
+  def shuffle(deck) do
+    Enum.shuffle(deck)
+  end
+
+  def contains?(deck, card) do
+    Enum.member?(deck, card)
   end
 end
