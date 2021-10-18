e355a549c416eec5ef9a9020ebcb9c844cc63880
Francisco Guilherme <francisco.antunes@pravaler.com.br>
2021-10-18 08:57:33 -0300

    feat(poc): implementacao inicial do fluxo de criacao de um 'topic'.

--- discuss/lib/discuss_web/controllers/topic_controller.ex
+++ discuss/lib/discuss_web/controllers/topic_controller.ex
@@ -6,6 +6,9 @@ defmodule DiscussWeb.TopicController do
   def new(conn, params) do
     changeset = Topic.changeset(%Topic{}, %{})
 
-    render(conn, "new.html")
+    render(conn, "new.html", changeset: changeset)
+  end
+
+  def create(conn, %{"topic" = topic}) do
   end
 end
