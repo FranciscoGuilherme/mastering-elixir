b94df8a182f8a9079c4fbb12249d36222481c14e
Francisco Guilherme <francisco.antunes@pravaler.com.br>
2021-10-21 08:29:09 -0300

    feat(poc): implementa metodologia de salvamento de usuarios na base.

--- discuss/lib/discuss_web/controllers/auth_controller.ex
+++ discuss/lib/discuss_web/controllers/auth_controller.ex
@@ -2,11 +2,20 @@ defmodule DiscussWeb.AuthController do
   use DiscussWeb, :controller
   plug Ueberauth
 
+  alias Discuss.Repo
   alias DiscussWeb.Models.User
 
   def callback(%{assigns: %{ueberauth_auth: auth}} = conn, params) do
     user_params = %{token: auth.credentials.token, email: auth.info.email, provider: "github"}
-
     changeset = User.changeset(%User{}, user_params)
+
+    insert_or_update_user(User.changeset(%User{}, user_params))
+  end
+
+  defp insert_or_update_user(changeset) do
+    case Repo.get_by(User, email: changeset.changes.email) do
+      nil -> Repo.insert(changeset) # {:ok, user}
+      user -> {:ok, user}
+    end
   end
 end
