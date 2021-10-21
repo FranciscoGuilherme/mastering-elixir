224a48cd4df9bc26048c391301f6880a3a28cfea
Francisco Guilherme <francisco.antunes@pravaler.com.br>
2021-10-21 08:49:25 -0300

    feat(poc): finaliza o fluxo de login atraves do GitHub.

--- discuss/lib/discuss_web/controllers/auth_controller.ex
+++ discuss/lib/discuss_web/controllers/auth_controller.ex
@@ -9,7 +9,21 @@ defmodule DiscussWeb.AuthController do
     user_params = %{token: auth.credentials.token, email: auth.info.email, provider: "github"}
     changeset = User.changeset(%User{}, user_params)
 
-    insert_or_update_user(User.changeset(%User{}, user_params))
+    signin(conn, changeset)
+  end
+
+  defp signin(conn, changeset) do
+    case insert_or_update_user(changeset) do
+      {:ok, user} ->
+        conn
+        |> put_flash(:info, "Welcome back!")
+        |> put_session(:user_id, user.id) # manipulacao de cookies
+        |> redirect(to: Routes.topic_path(conn, :index))
+      {:error, _reason} ->
+        conn
+        |> put_flash(:error, "Error signing in")
+        |> redirect(to: Routes.topic_path(conn, :index))
+    end
   end
 
   defp insert_or_update_user(changeset) do
